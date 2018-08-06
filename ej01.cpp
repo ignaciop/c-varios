2045472d5dd37e2371a627cbb01faad61c1b5d32
Ignacio Poggi <ignaciop.3@gmail.com>
2018-08-06 16:28:49 -0300

    FOD

--- ej01.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-	3+4;
-	return 0;
-}
--- median.h
+++
@@ -1,8 +0,0 @@
-#ifndef GUARD_MEDIAN_H
-#define GUARD_MEDIAN_H
- 
-// median.h - final version
-#include <vector>
-double median(std::vector<double>);
- 
-#endif // GUARD_MEDIAN_H
