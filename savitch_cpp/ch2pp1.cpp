f516d3ca55ee4ac6065aa99e7f3c59daf00699c8
Ignacio Poggi <ignaciop.3@gmail.com>
2022-10-05 11:05:17 -0300

    change

--- savitch_cpp/ch2pp1.cpp
+++ savitch_cpp/ch2pp1.cpp
@@ -3,21 +3,6 @@
 using namespace std;
 
 int main() {
-    char c;
-
-    cout << "Enter a character: ";
-    cin >> c;
-
-    cout << "\n";
-    cout << "    " << c << " " << c << " " << c << "\n";
-    cout << "   " << c << "    " << c << "\n";
-    cout << "  " << c << "\n";
-    cout << "  " << c << "\n";
-    cout << "  " << c << "\n";
-    cout << "  " << c << "\n";
-    cout << "  " << c << "\n";
-    cout << "   " << c << "    " << c << "\n";
-    cout << "    " << c << " " << c << " " << c << "\n";
 
     return 0;
 }
