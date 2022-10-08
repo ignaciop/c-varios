b3dec112105d28a7f711f7ddd2309dbcc6d8741a
Ignacio Poggi <ignaciop.3@gmail.com>
2022-10-08 19:48:11 -0300

    chapter 1 finished

---
+++ liang/ch1e4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "p\tp*5\tp*10\n";
+    cout << "5\t25\t50\n";
+    cout << "10\t50\t100\n";
+    cout << "25\t125\t250\n";
+    cout << "50\t250\t500\n";
+
+    return 0;
+}
---
+++ liang/ch1e9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Area of square of side 5.2: "
+            << 5.2 * 5.2 << "\n";
+
+    cout << "Perimeter of square of side 9.2: "
+            << 4 * 5.2 << "\n";
+
+    return 0;
+}
