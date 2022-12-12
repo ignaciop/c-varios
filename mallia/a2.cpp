fb993ac5103af31e7124c39bb391a6d5c1001d06
Ignacio <ignaciop.3@gmail.com>
2022-12-12 15:42:36 -0300

    ch2

--- mallia/a2.cpp
+++ mallia/a2.cpp
@@ -6,6 +6,7 @@ int main() {
     for (unsigned x = 0; x < 3; x++) {
         for (unsigned y = 0; y < 3; y++) {
             m[x][y] = x * y;
+            
             std::cout << m[x][y] << " ";
         }
         
