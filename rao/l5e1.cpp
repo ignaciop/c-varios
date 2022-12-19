cf88d336068e7c35032e8d9d74c8f46161d4bddb
Ignacio Poggi <ignaciop.3@gmail.com>
2022-12-19 02:40:25 -0300

    l5

---
+++ rao/l5e1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter a number: ";
+    int number;
+    std::cin >> number;
+
+    int result = ((number << 1) + 5) << 1;
+
+    std::cout << result << std::endl;
+    
+    return 0;
+}
+
+
