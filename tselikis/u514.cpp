79fbc0b97c13924811f6d91be3990468ee43c090
Ignacio Poggi <ignaciop.3@gmail.com>
2023-01-02 04:42:01 -0300

    u514 done!

---
+++ tselikis/u514.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter a month number: " << std::endl;
+    int month;
+    std::cin >> month;
+
+    int days_in_month = 0;
+
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            days_in_month = 31;
+            break;
+        case 2:
+            std::cout << "28 or 29 days in February? ";
+            std::cin >> days_in_month;
+            break;
+        default:
+            days_in_month = 30;
+            break;
+    }
+
+    std::cout << "Enter a day number: " << std::endl;
+    int day;
+    std::cin >> day;
+
+    int final_days = day + 50;
+    final_days %= days_in_month;
+
+    int final_month = month + 5/3;
+    
+    std::cout << final_month << "/" << final_days << std::endl;
+
+    return 0;
+}
---
+++ tselikis/u516.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter a numerator and denominator: " << std::endl;
+    int i, j;
+    std::cin >> i >> j;
+
+    std::cout << "Enter another numerator and denominator: " << std::endl;
+    int k, l;
+    std::cin >> k >> l;
+
+    
+    
+    return 0;
+}
