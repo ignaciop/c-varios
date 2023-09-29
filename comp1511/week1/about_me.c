7a71c698b618891f6d28373b77130740878a1ba7
Ignacio Poggi <ignaciop.3@gmail.com>
2023-09-29 03:10:13 -0300

    week1 finished

---
+++ comp1511/week1/about_me.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("My favourite food is: Burgers.\nI'm doing COMP1511 because: "
+            "I like to learn C.\nI hope to have achieved: proficiency by the "
+            "time I finish COMP1511.\nSomething I'm passionate about is: "
+            "Physics.\nMy question about COMP1511 that hasn't been answered "
+            "is: who started it.\n");
+    
+    return 0;
+}
---
+++ comp1511/week1/bad_pun.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, it is good to C you!\n");
+    
+    return 0;
+}
