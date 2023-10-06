7884b2ef1bb505eebf1ddb60397a039245e989ea
Ignacio Poggi <ignaciop.3@gmail.com>
2023-10-06 04:35:09 -0300

    e16

---
+++ uppsala/e16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void op_double(int *a);
+void op_reset(int *a);
+void op_invert(int *a);
+void applyTo(void (*func)(int *), int *tab, int size);
+
+int main() {
+    
+    
+    return 0;
+}
