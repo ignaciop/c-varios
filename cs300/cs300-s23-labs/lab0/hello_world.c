5ddbd33325dea79c405bcd5ab0d89d4e52ecdce2
Ignacio Poggi <ignaciop.3@gmail.com>
2023-10-11 20:31:29 -0300

    makefiles examples and cleaning

--- cs300/cs300-s23-labs/lab0/hello_world.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  // printf() displays the string inside quotation
-  printf("Oh, Shelley? She sells seashells by the shilling. Silly, selling Shelley.\n");
-
-  return 0;
-}
