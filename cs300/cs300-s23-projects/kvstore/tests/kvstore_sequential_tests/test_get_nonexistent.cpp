e2f169cf793cd0530f8b0c0b6014e9c06dc33909
Ignacio Poggi <ignaciop.3@gmail.com>
2023-10-11 00:35:12 -0300

    clean

--- cs300/cs300-s23-projects/kvstore/tests/kvstore_sequential_tests/test_get_nonexistent.cpp
+++
@@ -1,9 +0,0 @@
-#include "test_utils/test_utils.hpp"
-
-int main(int argc, char* argv[]) {
-  auto store = make_kvstore(argc, argv);
-
-  auto req = GetRequest{"nonexistent"};
-  auto res = GetResponse{};
-  ASSERT(!store->Get(&req, &res));
-}
