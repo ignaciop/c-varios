4bc934f88fcaf2b29c586a6cb9c3d3b110820f43
Ignacio Poggi <ignaciop.3@gmail.com>
2023-10-20 23:29:37 -0300

    cleaning

--- dive/e114.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int result = 1 + 2;
-
-    printf("%d\n", result);
-
-    return 0;
-}
--- dive/e1317.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    for (int i = 5; i < 10; i += 2) {
-        printf("yay!\n");
-    }
-    
-    return 0;
-}
