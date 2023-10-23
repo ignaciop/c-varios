e81ff35cde0d1c14da534b8d92bcffe836166212
Ignacio Poggi <ignaciop.3@gmail.com>
2023-10-23 14:05:35 -0300

    clean

--- deitel/ch2/c2e20.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Diameter of circle of radius 2: %d\n", 2 * 2);
-    printf("Circumference of circle of radius 2: %f\n", 2 * 3.14159f * 2);
-    printf("Area of circle of radius 2: %f\n", 3.14159f * 2 * 2);
-    
-    return 0;
-}
--- deitel/ch2/c2e21.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("%s", "*\n**\n***\n****\n*****\n");
-    
-    return 0;
-}
