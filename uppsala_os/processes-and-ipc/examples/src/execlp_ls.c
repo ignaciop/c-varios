0f9c36eaa742eab6709e70aa4199e05f14eb1f2a
Ignacio Poggi <ignaciop.3@gmail.com>
2023-12-19 03:56:08 -0300

    refactor

--- uppsala_os/processes-and-ipc/examples/src/execlp_ls.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h> // execlp()
-#include <stdio.h>  // perror()
-#include <stdlib.h> // EXIT_SUCCESS, EXIT_FAILURE
-
-int main(void) {
-  execlp("ls", "ls", "-l", NULL);
-  perror("Return from execlp() not expected");
-  exit(EXIT_FAILURE);
-}
