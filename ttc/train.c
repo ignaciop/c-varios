bd169b23ecd43e3499c5c2aecef3cd9cc860c1c4
ignaciop <ignaciop@users.noreply.github.com>
2023-12-26 23:50:05 -0300

    Update train.c

--- ttc/train.c
+++ ttc/train.c
@@ -62,7 +62,7 @@ int train_at_position(struct train *first, int pos) {
 }
 
 void prepend_train(struct train **first, struct train *new_node) {
-	if (*first == NULL && new_node == NULL) {
+	if (*first == NULL || new_node == NULL) {
 		fprintf(stderr, "Both trains are null.\n");
 		
 		exit(EXIT_FAILURE);
