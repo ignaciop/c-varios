455027f46af355372c7f893769094cd6a0bc0139
Ignacio Poggi <ignaciop.3@gmail.com>
2024-01-14 23:55:47 -0300

    cmpu334 threads moved

---
+++ threads/cmpu334/train.c
@@ -0,0 +1,17 @@
+#include "train.h"
+
+void station_init(struct station *station) {
+	return;
+}
+
+void station_load_train(struct station *station, int count) {
+	return;
+}
+
+void station_wait_for_train(struct station *station) {
+	return;
+}
+
+void station_on_board(struct station *station) {
+	return;
+}
